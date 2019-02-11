ff635f7cad258e89f570885efffb416fa077884f
cicada-orni <47474010+cicada-orni@users.noreply.github.com>
2019-02-11 07:45:58 -0800

    Add files via upload

---
+++ guessing_game 1.py
@@ -0,0 +1,38 @@
+import random
+
+def startGame():
+    secret_number = random.randrange(1, 101)
+    guess = 0
+    attempts = 0
+    print("Welcome to the Number Guessing Game")
+    name = input("What is your name?  ")
+    print("Hi {}, In this game you have to Guess the number between 1 and 100 ".format(name))
+    while guess != "A":
+        try:
+            guess = int(input("Enter the guess\n"))
+            attempts += 1
+            if guess < 1:
+                print("Sorry, invalid value. Guess the number between 1 and 100")
+            elif guess > 100:
+                print ("Sorry, invalid value. Guess the number between 1 and 100")
+            if guess > secret_number:
+                print("Guess should be a lower number")
+            elif guess < secret_number:
+                print("Guess should be a higher number")
+            else:
+                print("Congratulations ! You got it")
+                print("The total number of attempts are {}.".format(attempts))
+                continueGame = input("Would you like to play again? \n Press 'Y' to continue or 'N' to quit ")
+                if continueGame.upper() == 'Y':
+                    print("Welcome Back to the Number Guessing Game.")
+                    continue
+                else:
+                    print("Good Bye till next time")
+                    break
+        except ValueError:
+            print("Sorry, This is a invalid value. Please try again")
+        else:
+            continue
+
+if __name__ == '__main__':
+    startGame()
